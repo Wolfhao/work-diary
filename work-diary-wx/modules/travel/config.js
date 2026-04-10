2bcbde1212ced3ee9e3e4ff2cbfb32175d9224dd
ZhangZhiHao <1057251715@qq.com>
2026-04-11 03:38:47 +0800

    小程序重构

--- work-diary-wx/modules/travel/config.js
+++ work-diary-wx/modules/travel/config.js
@@ -4,7 +4,7 @@ export default {
       id: 'pingyao',
       city: '平遥古城',
       title: '两日浪漫之旅',
-      cover: 'https://www.suntool.online/file/download?key=static/pingyao.png',
+      cover: 'https://www.suntool.online/file/download?key=static/pingyao.jpg',
       intro: '漫步千年古城，感受晋商文化，在古城墙上留下我们的爱情誓言。',
       date: '2025年6月'
     }
